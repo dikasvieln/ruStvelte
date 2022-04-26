19bc948970f72406b12f2e726d01bc5495e2409b
Aulia Mahardika <aulia.mahardika@live.com>
2022-04-26 23:39:51 +0700

    update witch actix framework

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 //#![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
 //#[macro_use] extern crate serde_derive;
 
-//use rocket::http::{Cookie};
-use rocket::response::content::Json;
-//use serde::Serialize;
-use rocket::response::{content, status};
-use rocket::http::Status;
 
 //mod categories;
 
@@ -26,20 +20,33 @@ fn api() -> String {
     })
 }*/
 
-#[derive(Responder)]
-#[response(status = 200, content_type = "json")]
-struct Success(&'static str);
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-fn json() -> Success {
-    Success("{ \"hi\": \"world\"  }")
+async fn hello() -> impl Responder {
+        HttpResponse::Ok().body("Hello world!")
 }
 
-#[launch]
-fn rocket() -> _  {
-    //categories::categories();
-    rocket::build()
-        .mount("/", routes![json])
-
-    // println!("Hello, world!");
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+        HttpResponse::Ok().body(req_body)
 }
+
+async fn manual_hello() -> impl Responder {
+        HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
