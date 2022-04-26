b16ffbfc83d244fbd2d38fa18b7376819921d9b6
Aulia Mahardika <aulia.mahardika@live.com>
2022-04-26 22:55:58 +0700

    refixing folder and cli

---
+++ packages/raftel/README.md
@@ -0,0 +1,32 @@
+mycli
+=====
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/mycli.svg)](https://npmjs.org/package/mycli)
+[![Downloads/week](https://img.shields.io/npm/dw/mycli.svg)](https://npmjs.org/package/mycli)
+[![License](https://img.shields.io/npm/l/mycli.svg)](https://github.com/sw-yx/mycli/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g 
+$ mycli COMMAND
+running command...
+$ mycli (-v|--version|version)
+mycli/0.0.0 darwin-x64 node-v10.17.0
+$ mycli --help [COMMAND]
+USAGE
+  $ mycli COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ packages/raftel/src/commands/init.ts
@@ -0,0 +1,61 @@
+import { flags } from "@oclif/command";
+import Base from "../base";
+var debug = require("debug")("raftel:init");
+const { prompt } = require("enquirer");
+const copy = require("copy-template-dir");
+const path = require("path");
+
+class Raftel extends Base {
+  static description = "describe the command here";
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({
+      char: "n",
+      description: "name to print"
+    }),
+    // flag with no value (-f, --force)
+    force: flags.boolean({ char: "f" })
+  };
+
+  static args = [{ name: "file" }];
+  static strict = false;
+  async run() {
+    const { args, flags } = this.parse(Raftel);
+    // debug("parsing args", args);
+    // debug("parsing flags", flags);
+
+    if (typeof flags.name === "undefined") {
+      if (this.config && this.config.name) {
+        flags.name = this.config.name;
+      } else {
+        flags.name = await prompt({
+          type: "input",
+          name: "name",
+          message: "What is the folder name?"
+        })
+          .then(({ name }: { name: string }) => name)
+          .catch(console.error)
+          .finally(() =>
+            console.log("You can specify this with the --name flag in future")
+          );
+      }
+    }
+    const name = flags.name;
+
+    const vars = { projectName: name };
+    const inDir = path.resolve(__dirname, "../templates/ruStvelte");
+    const outDir = path.join(process.cwd(), name);
+
+    copy(inDir, outDir, vars, (err: Error, createdFiles: string[]) => {
+      if (err) throw err;
+      createdFiles.forEach(filePath => console.log(`Created ${filePath}`));
+      console.log("done!");
+    });
+  }
+}
+
+export = Raftel;
